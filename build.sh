dac659c99d89682ae621b0814d1cf9f21b529a13
Koji Ishiwatari <koji.ishiwatari@gmail.com>
2022-01-30 14:26:14 +0900

    1st commit.

---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+###
+fname=FD-3.01j.tar.gz
+tar_fname=${fname%.*}
+dir=${tar_fname%.*}
+
+###
+url=http://hp.vector.co.jp/authors/VA012337/soft/fd
+
+###
+if [ -e $fname ]; then
+    echo "Info : Already file exist."
+else
+    wget ${url}/${fname}
+fi
+
+###
+tar zxvf $fname
+rm -rf $fname
+
+###
+cd ${dir}
+
+patch < ../patch/fd.spec
+
+cd ..
+
+###
+tar cvf ${tar_fname} ${dir}
+gzip -9 ${tar_fname}
+
+###
+rm -rf ${dir}
+
+###
+rpmbuild -ta ${fname}
